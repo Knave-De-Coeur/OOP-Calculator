405be76f622016aa47b8024b1d067af79144f80e
Alexander Mifsud <alexanderm.1496@gmail.com>
2018-09-05 12:27:10 +0200

    Adding somme more files and directories as well as beginning the form

---
+++ includes/calculation.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexa
+ * Date: 05/09/2018
+ * Time: 12:15
+ */
+class Calculation {
+
+}
--- index.php
+++ index.php
@@ -5,3 +5,29 @@
  * Date: 05/09/2018
  * Time: 11:49
  */
+include "includes/Calculation.inc.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>OOP-Calculator</title>
+</head>
+<body>
+<h1 class="heading">Calculator</h1>
+    <form action="processing/process.php" method="post">
+        <input type="text" name="first_number" placeholder="First Number"/>
+        <br />
+        <input type="text" name="second_number" placeholder="Second Number" />
+        <br />
+        <select name="operation" id="">
+            <option value="add">Add</option>
+            <option value="subtract">Subtract</option>
+            <option value="multiply">Multiply</option>
+            <option value="divide">Divide</option>
+        </select>
+        <input type="submit" value="Calculate" />
+    </form>
+</body>
+</html>
